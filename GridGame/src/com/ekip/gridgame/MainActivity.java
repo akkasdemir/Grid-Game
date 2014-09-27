23115843387cc7864a8bb3bb5d838245bd9e527f
Abdurrahman Akkas <falafelremzi@gmail.com>
2014-09-27 02:45:49 -0400

    Initial Commit Hayırlı uğurlu olsun

---
+++ GridGame/src/com/ekip/gridgame/MainActivity.java
@@ -0,0 +1,19 @@
+/*
+ * Evet gençler, oyun vatana millete hayırlı uğurlu olsun,
+ * Javadoc'u öğrenin, yazdığınız "her fonksiyona" documentation yazın
+ * Documentation yazmayanı oyarım
+ * Bi de branch yapmayın, tek koldan yürüyelim
+ */
+package com.ekip.gridgame;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
